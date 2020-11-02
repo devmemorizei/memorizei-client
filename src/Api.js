91cefaa0bcb0b908973e050a14eaf03f8101796f
jonas.fonseca <dornelasjonas96@hotmail.com>
2020-11-02 13:32:54 -0300

    Ajuste na url da api

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const urlBase = 'https://memorizei-api.herokuapp.com/api';
-const urlBaseDev = 'http://localhost:8090/api';
+//const urlBaseDev = 'http://localhost:8090/api';
 
 const createUser = async (user) => {
-    return axios.post(`${urlBaseDev}/user`, user)
+    return axios.post(`${urlBase}/user`, user)
     .then(function (response) {
         return response;
     })
